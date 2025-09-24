734c81fb93f447acd03fab47526121356a3d8ec0
optikrus <optikrus@MacBook-Air-Kirill.local>
2025-09-24 22:27:18 +0300

    add permissions to /competencies

--- src/tests/api/test_competencies.py
+++ src/tests/api/test_competencies.py
@@ -14,6 +14,7 @@ from src.core.competencies.use_cases import (
     RemoveSkillFromCompetencyUseCase,
     UpdateCompetencyUseCase,
 )
+from src.core.exceptions import PermissionDeniedError
 from src.tests.fixtures import APIFixture, ContainerFixture, FactoryFixture
 
 
@@ -22,9 +23,23 @@ class TestCreateCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(CreateCompetencyUseCase)
 
+    def test_not_auth(self) -> None:
+        response = self.api.create_competency(name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.create_competency(name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
     def test_create_competency(self) -> None:
         self.use_case.execute.return_value = self.factory.competency(
-            competency_id=1, name="ML", max_level=100
+            competency_id=1,
+            name="ML",
+            max_level=100,
         )
 
         response = self.hr_api.create_competency(name="ML", max_level=100)
@@ -53,7 +68,13 @@ class TestGetCompetenciesAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(GetCompetenciesUseCase)
 
-    async def test_get_competencies(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.get_competencies()
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_get_competencies(self) -> None:
         self.use_case.execute.return_value = self.factory.competencies(
             values=[
                 self.factory.competency(competency_id=1, name="ML", max_level=100),
@@ -61,7 +82,7 @@ class TestGetCompetenciesAPI(APIFixture, FactoryFixture, ContainerFixture):
             ]
         )
 
-        response = self.api.get_competencies()
+        response = self.hr_api.get_competencies()
 
         assert response.status_code == codes.OK
         assert response.json() == {
@@ -72,34 +93,50 @@ class TestGetCompetenciesAPI(APIFixture, FactoryFixture, ContainerFixture):
         }
         self.use_case.execute.assert_called_once()
 
+    def test_get_competencies_empty(self) -> None:
+        self.use_case.execute.return_value = self.factory.competencies(values=[])
+
+        response = self.candidate_api.get_competencies()
+
+        assert response.status_code == codes.OK
+        assert response.json() == {"values": []}
+        self.use_case.execute.assert_called_once()
+
 
 class TestGetCompetencyDetailAPI(APIFixture, FactoryFixture, ContainerFixture):
     @pytest.fixture(autouse=True)
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(GetCompetencyDetailUseCase)
 
-    async def test_get_competency_by_id(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.get_competency(competency_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_get_competency_by_id(self) -> None:
         self.use_case.execute.return_value = self.factory.competency(
-            competency_id=1, name="ML", max_level=100
+            competency_id=1,
+            name="ML",
+            max_level=100,
         )
 
-        response = self.api.get_competency(competency_id=1)
+        response = self.hr_api.get_competency(competency_id=1)
 
         assert response.status_code == codes.OK
         assert response.json() == {"id": 1, "name": "ML", "maxLevel": 100, "skills": []}
         self.use_case.execute.assert_called_once()
         self.use_case.execute.assert_awaited_once_with(competency_id=1)
 
-    async def test_get_competency_not_found(self) -> None:
-        use_case = await self.container.override_use_case(GetCompetencyDetailUseCase)
-        use_case.execute.side_effect = CompetencyNotFoundError
+    def test_get_competency_not_found(self) -> None:
+        self.use_case.execute.side_effect = CompetencyNotFoundError
 
-        response = self.api.get_competency(competency_id=999)
+        response = self.candidate_api.get_competency(competency_id=999)
 
         assert response.status_code == codes.NOT_FOUND
         assert response.json() == {"detail": CompetencyNotFoundError.detail}
-        use_case.execute.assert_called_once()
-        use_case.execute.assert_awaited_once_with(competency_id=999)
+        self.use_case.execute.assert_called_once()
+        self.use_case.execute.assert_awaited_once_with(competency_id=999)
 
 
 class TestUpdateCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
@@ -107,9 +144,23 @@ class TestUpdateCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(UpdateCompetencyUseCase)
 
-    async def test_update_competency(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.update_competency(competency_id=1, name="TEST", max_level=150)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.update_competency(competency_id=1, name="TEST", max_level=150)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
+    def test_update_competency(self) -> None:
         self.use_case.execute.return_value = self.factory.competency(
-            competency_id=1, name="ML Advanced", max_level=150
+            competency_id=1,
+            name="ML Advanced",
+            max_level=150,
         )
 
         response = self.hr_api.update_competency(competency_id=1, name="ML Advanced", max_level=150)
@@ -120,7 +171,7 @@ class TestUpdateCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
             competency=self.factory.competency(competency_id=1, name="ML Advanced", max_level=150)
         )
 
-    async def test_update_competency_name_already_exists(self) -> None:
+    def test_update_competency_name_already_exists(self) -> None:
         self.use_case.execute.side_effect = CompetencyNameAlreadyExistError
 
         response = self.hr_api.update_competency(competency_id=1, name="ML Advanced", max_level=150)
@@ -131,7 +182,7 @@ class TestUpdateCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
             competency=self.factory.competency(competency_id=1, name="ML Advanced", max_level=150)
         )
 
-    async def test_update_competency_not_found(self) -> None:
+    def test_update_competency_not_found(self) -> None:
         self.use_case.execute.side_effect = CompetencyNotFoundError
 
         response = self.hr_api.update_competency(competency_id=999, name="Unknown", max_level=10)
@@ -149,7 +200,19 @@ class TestDeleteCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(DeleteCompetencyUseCase)
 
-    async def test_delete_competency(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.delete_competency(competency_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.delete_competency(competency_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
+    def test_delete_competency(self) -> None:
         self.use_case.execute.return_value = None
 
         response = self.hr_api.delete_competency(competency_id=1)
@@ -158,7 +221,7 @@ class TestDeleteCompetencyAPI(APIFixture, FactoryFixture, ContainerFixture):
         self.use_case.execute.assert_called_once()
         self.use_case.execute.assert_awaited_once_with(competency_id=1)
 
-    async def test_delete_competency_not_found(self) -> None:
+    def test_delete_competency_not_found(self) -> None:
         self.use_case.execute.side_effect = CompetencyNotFoundError
 
         response = self.hr_api.delete_competency(competency_id=999)
@@ -177,6 +240,30 @@ class TestCompetencySkillsAPI(APIFixture, FactoryFixture, ContainerFixture):
             RemoveSkillFromCompetencyUseCase
         )
 
+    def test_add_skill_to_competency_not_auth(self) -> None:
+        response = self.api.add_skill_to_competency(competency_id=1, skill_id=10)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_add_skill_to_competency_candidate_forbidden(self) -> None:
+        response = self.candidate_api.add_skill_to_competency(competency_id=1, skill_id=10)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
+    def test_remove_skill_from_competency_not_auth(self) -> None:
+        response = self.api.remove_skill_from_competency(competency_id=1, skill_id=10)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_remove_skill_from_competency_candidate_forbidden(self) -> None:
+        response = self.candidate_api.remove_skill_from_competency(competency_id=1, skill_id=10)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
     async def test_add_skill_to_competency(self) -> None:
         self.add_use_case.execute.return_value = self.factory.competency(
             competency_id=1,
@@ -192,10 +279,12 @@ class TestCompetencySkillsAPI(APIFixture, FactoryFixture, ContainerFixture):
 
     async def test_remove_skill_from_competency(self) -> None:
         self.remove_use_case.execute.return_value = self.factory.competency(
-            competency_id=1, name="ML", max_level=100
+            competency_id=1,
+            name="ML",
+            max_level=100,
         )
 
-        response = self.api.remove_skill_from_competency(competency_id=1, skill_id=10)
+        response = self.hr_api.remove_skill_from_competency(competency_id=1, skill_id=10)
 
         assert response.status_code == codes.OK
         self.remove_use_case.execute.assert_called_once()
