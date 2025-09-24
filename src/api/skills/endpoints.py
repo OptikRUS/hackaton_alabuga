0c3410435a53181f4f5ac708778cf128cfeeba93
optikrus <optikrus@MacBook-Air-Kirill.local>
2025-09-25 01:15:36 +0300

    add permissions to /skills

--- src/api/skills/endpoints.py
+++ src/api/skills/endpoints.py
@@ -1,6 +1,8 @@
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
 from fastapi import APIRouter, status
 
+from src.api.auth.schemas import JwtHRUser, JwtUser
+from src.api.openapi import openapi_extra
 from src.api.skills.schemas import (
     SkillCreateRequest,
     SkillResponse,
@@ -20,68 +22,83 @@ router = APIRouter(tags=["skills"], route_class=DishkaRoute)
 
 @router.post(
     path="/skills",
+    openapi_extra=openapi_extra,
     status_code=status.HTTP_201_CREATED,
     summary="Создать навык",
     description="Создает новый навык в системе",
 )
 async def create_skill(
+    user: FromDishka[JwtHRUser],
     body: SkillCreateRequest,
     use_case: FromDishka[CreateSkillUseCase],
 ) -> SkillResponse:
+    _ = user
     skill = await use_case.execute(skill=body.to_schema())
     return SkillResponse.from_schema(skill=skill)
 
 
 @router.get(
     path="/skills",
+    openapi_extra=openapi_extra,
     status_code=status.HTTP_200_OK,
     summary="Получить список навыков",
     description="Возвращает все доступные навыки",
 )
 async def get_skills(
+    user: FromDishka[JwtUser],
     use_case: FromDishka[GetSkillsUseCase],
 ) -> SkillsResponse:
+    _ = user
     skills = await use_case.execute()
     return SkillsResponse.from_schema(skills=skills)
 
 
 @router.get(
     path="/skills/{skill_id}",
+    openapi_extra=openapi_extra,
     status_code=status.HTTP_200_OK,
     summary="Получить навык по ID",
     description="Возвращает детальную информацию о навыке",
 )
 async def get_skill(
     skill_id: int,
+    user: FromDishka[JwtUser],
     use_case: FromDishka[GetSkillDetailUseCase],
 ) -> SkillResponse:
+    _ = user
     skill = await use_case.execute(skill_id=skill_id)
     return SkillResponse.from_schema(skill=skill)
 
 
 @router.put(
     path="/skills/{skill_id}",
+    openapi_extra=openapi_extra,
     status_code=status.HTTP_200_OK,
     summary="Обновить навык",
     description="Обновляет данные указанного навыка",
 )
 async def update_skill(
     skill_id: int,
+    user: FromDishka[JwtHRUser],
     body: SkillUpdateRequest,
     use_case: FromDishka[UpdateSkillUseCase],
 ) -> SkillResponse:
+    _ = user
     skill = await use_case.execute(skill=body.to_schema(skill_id=skill_id))
     return SkillResponse.from_schema(skill=skill)
 
 
 @router.delete(
     path="/skills/{skill_id}",
+    openapi_extra=openapi_extra,
     status_code=status.HTTP_204_NO_CONTENT,
     summary="Удалить навык",
     description="Удаляет указанный навык",
 )
 async def delete_skill(
     skill_id: int,
+    user: FromDishka[JwtHRUser],
     use_case: FromDishka[DeleteSkillUseCase],
 ) -> None:
+    _ = user
     await use_case.execute(skill_id=skill_id)
--- src/tests/api/test_skills.py
+++ src/tests/api/test_skills.py
@@ -1,6 +1,7 @@
 import pytest
 from httpx import codes
 
+from src.core.exceptions import PermissionDeniedError
 from src.core.skills.exceptions import SkillNameAlreadyExistError, SkillNotFoundError
 from src.core.skills.use_cases import (
     CreateSkillUseCase,
@@ -17,12 +18,26 @@ class TestCreateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(CreateSkillUseCase)
 
+    def test_not_auth(self) -> None:
+        response = self.api.create_skill(name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.create_skill(name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
     def test_create_skill(self) -> None:
         self.use_case.execute.return_value = self.factory.skill(
-            skill_id=1, name="Python", max_level=100
+            skill_id=1,
+            name="Python",
+            max_level=100,
         )
 
-        response = self.api.create_skill(name="Python", max_level=100)
+        response = self.hr_api.create_skill(name="Python", max_level=100)
 
         assert response.status_code == codes.CREATED
         assert response.json() == {"id": 1, "name": "Python", "maxLevel": 100}
@@ -34,7 +49,7 @@ class TestCreateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
     def test_create_skill_name_already_exists(self) -> None:
         self.use_case.execute.side_effect = SkillNameAlreadyExistError
 
-        response = self.api.create_skill(name="Python", max_level=100)
+        response = self.hr_api.create_skill(name="Python", max_level=100)
 
         assert response.status_code == codes.CONFLICT
         assert response.json() == {"detail": SkillNameAlreadyExistError.detail}
@@ -48,7 +63,13 @@ class TestGetSkillsAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(GetSkillsUseCase)
 
-    async def test_get_skills(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.get_skills()
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_get_skills(self) -> None:
         self.use_case.execute.return_value = self.factory.skills(
             values=[
                 self.factory.skill(skill_id=1, name="Python", max_level=100),
@@ -56,7 +77,7 @@ class TestGetSkillsAPI(APIFixture, FactoryFixture, ContainerFixture):
             ]
         )
 
-        response = self.api.get_skills()
+        response = self.hr_api.get_skills()
 
         assert response.status_code == codes.OK
         assert response.json() == {
@@ -67,34 +88,50 @@ class TestGetSkillsAPI(APIFixture, FactoryFixture, ContainerFixture):
         }
         self.use_case.execute.assert_called_once()
 
+    def test_get_skills_empty(self) -> None:
+        self.use_case.execute.return_value = self.factory.skills(values=[])
+
+        response = self.candidate_api.get_skills()
+
+        assert response.status_code == codes.OK
+        assert response.json() == {"values": []}
+        self.use_case.execute.assert_called_once()
+
 
 class TestGetSkillDetailAPI(APIFixture, FactoryFixture, ContainerFixture):
     @pytest.fixture(autouse=True)
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(GetSkillDetailUseCase)
 
-    async def test_get_skill_by_id(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.get_skill(skill_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_get_skill_by_id(self) -> None:
         self.use_case.execute.return_value = self.factory.skill(
-            skill_id=1, name="Python", max_level=100
+            skill_id=1,
+            name="Python",
+            max_level=100,
         )
 
-        response = self.api.get_skill(skill_id=1)
+        response = self.hr_api.get_skill(skill_id=1)
 
         assert response.status_code == codes.OK
         assert response.json() == {"id": 1, "name": "Python", "maxLevel": 100}
         self.use_case.execute.assert_called_once()
         self.use_case.execute.assert_awaited_once_with(skill_id=1)
 
-    async def test_get_skill_not_found(self) -> None:
-        use_case = await self.container.override_use_case(GetSkillDetailUseCase)
-        use_case.execute.side_effect = SkillNotFoundError
+    def test_get_skill_not_found(self) -> None:
+        self.use_case.execute.side_effect = SkillNotFoundError
 
-        response = self.api.get_skill(skill_id=999)
+        response = self.candidate_api.get_skill(skill_id=999)
 
         assert response.status_code == codes.NOT_FOUND
         assert response.json() == {"detail": SkillNotFoundError.detail}
-        use_case.execute.assert_called_once()
-        use_case.execute.assert_awaited_once_with(skill_id=999)
+        self.use_case.execute.assert_called_once()
+        self.use_case.execute.assert_awaited_once_with(skill_id=999)
 
 
 class TestUpdateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
@@ -102,12 +139,26 @@ class TestUpdateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(UpdateSkillUseCase)
 
-    async def test_update_skill(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.update_skill(skill_id=1, name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.update_skill(skill_id=1, name="TEST", max_level=100)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
+    def test_update_skill(self) -> None:
         self.use_case.execute.return_value = self.factory.skill(
-            skill_id=1, name="Python Advanced", max_level=150
+            skill_id=1,
+            name="Python Advanced",
+            max_level=150,
         )
 
-        response = self.api.update_skill(skill_id=1, name="Python Advanced", max_level=150)
+        response = self.hr_api.update_skill(skill_id=1, name="Python Advanced", max_level=150)
 
         assert response.status_code == codes.OK
         self.use_case.execute.assert_called_once()
@@ -115,10 +166,10 @@ class TestUpdateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
             skill=self.factory.skill(skill_id=1, name="Python Advanced", max_level=150)
         )
 
-    async def test_update_skill_name_already_exists(self) -> None:
+    def test_update_skill_name_already_exists(self) -> None:
         self.use_case.execute.side_effect = SkillNameAlreadyExistError
 
-        response = self.api.update_skill(skill_id=1, name="Python Advanced", max_level=150)
+        response = self.hr_api.update_skill(skill_id=1, name="Python Advanced", max_level=150)
 
         assert response.status_code == codes.CONFLICT
         self.use_case.execute.assert_called_once()
@@ -126,10 +177,10 @@ class TestUpdateSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
             skill=self.factory.skill(skill_id=1, name="Python Advanced", max_level=150)
         )
 
-    async def test_update_skill_not_found(self) -> None:
+    def test_update_skill_not_found(self) -> None:
         self.use_case.execute.side_effect = SkillNotFoundError
 
-        response = self.api.update_skill(skill_id=999, name="Unknown", max_level=10)
+        response = self.hr_api.update_skill(skill_id=999, name="Unknown", max_level=10)
 
         assert response.status_code == codes.NOT_FOUND
         assert response.json() == {"detail": SkillNotFoundError.detail}
@@ -144,19 +195,31 @@ class TestDeleteSkillAPI(APIFixture, FactoryFixture, ContainerFixture):
     async def setup(self) -> None:
         self.use_case = await self.container.override_use_case(DeleteSkillUseCase)
 
-    async def test_delete_skill(self) -> None:
+    def test_not_auth(self) -> None:
+        response = self.api.delete_skill(skill_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
+
+    def test_candidate_forbidden(self) -> None:
+        response = self.candidate_api.delete_skill(skill_id=1)
+
+        assert response.status_code == codes.FORBIDDEN
+        assert response.json() == {"detail": PermissionDeniedError.detail}
+
+    def test_delete_skill(self) -> None:
         self.use_case.execute.return_value = None
 
-        response = self.api.delete_skill(skill_id=1)
+        response = self.hr_api.delete_skill(skill_id=1)
 
         assert response.status_code == codes.NO_CONTENT
         self.use_case.execute.assert_called_once()
         self.use_case.execute.assert_awaited_once_with(skill_id=1)
 
-    async def test_delete_skill_not_found(self) -> None:
+    def test_delete_skill_not_found(self) -> None:
         self.use_case.execute.side_effect = SkillNotFoundError
 
-        response = self.api.delete_skill(skill_id=999)
+        response = self.hr_api.delete_skill(skill_id=999)
 
         assert response.status_code == codes.NOT_FOUND
         assert response.json() == {"detail": SkillNotFoundError.detail}
